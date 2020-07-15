2b91d147774326df321b360dd1306f31cdbf3cc1
IamCathal <cathalsocallaghan@gmail.com>
2020-07-15 15:54:51 +0100

    Add better handling of requests with HTTP status codes and content-type

--- backend/main.go
+++ backend/main.go
@@ -13,9 +13,23 @@ import (
 	"github.com/shurcooL/graphql"
 )
 
+type response struct {
+	Status string `json:"status"`
+	Body   string `json:"body"`
+}
+
 func homeHandler(w http.ResponseWriter, req *http.Request) {
+	startTime := time.Now().UnixNano() / int64(time.Millisecond)
+
+	res := response{
+		Status: "success",
+		Body:   "Home page",
+	}
+
+	logCall("GET", "/", "200", startTime)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Working home directory")
+	json.NewEncoder(w).Encode(res)
 }
 
 func getFellowHandler(w http.ResponseWriter, req *http.Request) {
@@ -29,14 +43,28 @@ func getFellowHandler(w http.ResponseWriter, req *http.Request) {
 	if !authorized {
 		endPoint := fmt.Sprintf("/getfellow/%s", vars["username"])
 		logCall("POST", endPoint, "401", startTime)
-		fmt.Fprintf(w, "You are not authorized to use this API")
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		res := response{
+			Status: "error",
+			Body:   "Incorrect secret given, you are not authorized to use this API",
+		}
+		json.NewEncoder(w).Encode(res)
 		return
 	}
 
 	if vars["username"] == "" {
 		endPoint := fmt.Sprintf("/getfellow/%s", vars["username"])
 		logCall("POST", endPoint, "400", startTime)
-		fmt.Fprintf(w, "No username given")
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		res := response{
+			Status: "error",
+			Body:   "No username given",
+		}
+		json.NewEncoder(w).Encode(res)
 		return
 	}
 
@@ -49,15 +77,11 @@ func getFellowHandler(w http.ResponseWriter, req *http.Request) {
 	err := client.Query(context.Background(), &tempStruct.repoContrib, nil)
 	CheckAPICallErr(err)
 
-	jsonData, err := json.Marshal(tempStruct.repoContrib)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	endPoint := fmt.Sprintf("/getfellow/%s", vars["username"])
 	logCall("POST", endPoint, "200", startTime)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, string(jsonData))
+	json.NewEncoder(w).Encode(tempStruct.repoContrib)
 
 }
 
@@ -67,41 +91,41 @@ func main() {
 		log.Fatal("No secret key set")
 	}
 
-	// r := mux.NewRouter()
-	// r.HandleFunc("/", homeHandler).Methods("GET")
-	// r.HandleFunc("/getfellow/{username}", getFellowHandler).Methods("POST")
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/getfellow/{username}", getFellowHandler).Methods("POST")
 
-	// http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", r)
 
-	httpClient := SetupOAuth()
-	client := graphql.NewClient("https://api.github.com/graphql", httpClient)
+	// httpClient := SetupOAuth()
+	// client := graphql.NewClient("https://api.github.com/graphql", httpClient)
 
-	var tempStruct megaJSONStruct
+	// var tempStruct megaJSONStruct
 
-	// Call the API with the relevant queries
-	err := client.Query(context.Background(), &tempStruct.repoContrib, nil)
-	CheckAPICallErr(err)
-	err = client.Query(context.Background(), &tempStruct.prMerged, nil)
-	CheckAPICallErr(err)
-	err = client.Query(context.Background(), &tempStruct.prOpened, nil)
-	CheckAPICallErr(err)
-	err = client.Query(context.Background(), &tempStruct.issOpened, nil)
-	CheckAPICallErr(err)
-	err = client.Query(context.Background(), &tempStruct.issClosed, nil)
-	CheckAPICallErr(err)
+	// // Call the API with the relevant queries
+	// err := client.Query(context.Background(), &tempStruct.repoContrib, nil)
+	// CheckAPICallErr(err)
+	// err = client.Query(context.Background(), &tempStruct.prMerged, nil)
+	// CheckAPICallErr(err)
+	// err = client.Query(context.Background(), &tempStruct.prOpened, nil)
+	// CheckAPICallErr(err)
+	// err = client.Query(context.Background(), &tempStruct.issOpened, nil)
+	// CheckAPICallErr(err)
+	// err = client.Query(context.Background(), &tempStruct.issClosed, nil)
+	// CheckAPICallErr(err)
 
-	err = client.Query(context.Background(), &tempStruct.PRContributions, nil)
-	CheckAPICallErr(err)
+	// err = client.Query(context.Background(), &tempStruct.PRContributions, nil)
+	// CheckAPICallErr(err)
 
-	err = client.Query(context.Background(), &tempStruct.PRCommits, nil)
-	CheckAPICallErr(err)
+	// err = client.Query(context.Background(), &tempStruct.PRCommits, nil)
+	// CheckAPICallErr(err)
 
-	err = client.Query(context.Background(), &tempStruct.accountInfo, nil)
-	CheckAPICallErr(err)
+	// err = client.Query(context.Background(), &tempStruct.accountInfo, nil)
+	// CheckAPICallErr(err)
 
-	fmt.Println(tempStruct.accountInfo)
+	// fmt.Println(tempStruct.accountInfo)
 
-	writeJSON(tempStruct)
+	// writeJSON(tempStruct)
 
 	// fmt.Println(tempStruct.prOpened.Search.IssueCount)
 	// fmt.Println(tempStruct.prMerged)
