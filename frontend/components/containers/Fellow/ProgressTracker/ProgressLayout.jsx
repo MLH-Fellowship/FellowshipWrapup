aa6430222f2af0afef43f229990bd85041377b0c
Kartik Choudhary <kartikc.918@gmail.com>
2020-07-16 22:20:22 +0530

    Add cursive font for nav logo

--- frontend/components/containers/Fellow/ProgressTracker/ProgressLayout.jsx
+++ frontend/components/containers/Fellow/ProgressTracker/ProgressLayout.jsx
@@ -8,7 +8,7 @@ export default function ProgressLayout() {
                 <h1 class="col-lg-12 display-3 text-left mt-5 section-heading">Progress compared to others in
                     your team
                 </h1>
-                <h1 class="bg-text mb-2">Progress Made</h1>
+                <h1 class="bg-text mb-2 pl-2">Progress Made</h1>
                 
                 <ProgressRound />
                 <ProgressTriangle />
