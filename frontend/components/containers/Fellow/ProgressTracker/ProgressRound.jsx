2b49e4404eb01beece2826c95322230512019644
Sebastian Crossa <crossasebastian@gmail.com>
2020-07-17 08:21:57 -0600

    feat: small fix

--- frontend/components/containers/Fellow/ProgressTracker/ProgressRound.jsx
+++ frontend/components/containers/Fellow/ProgressTracker/ProgressRound.jsx
@@ -38,19 +38,6 @@ export default function ProgressRound({ countIss, countPr, contributions }) {
             {contributions[1].PrimaryLanguage.Name}
           </h4>
         </div>
-
-        <div className="col-md-3 col-sm-6 pb-5">
-          <div className="progress-round blue">
-            <span className="progress-left">
-              <span className="progress-round-bar"></span>
-            </span>
-            <span className="progress-right">
-              <span className="progress-round-bar"></span>
-            </span>
-            <div className="progress-round-value">100%</div>
-          </div>
-          <h4 className="pt-4 text-center bold-text">Fun Had!</h4>
-        </div>
       </div>
     </div>
   );
